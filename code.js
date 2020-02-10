5f84baa4e00a91b5e695376efe1d4daffc14a6b6
Andrew Ball <andy.bob.ball@gmail.com>
2020-02-10 16:05:07 -0600

    Exerciese 9

--- code.js
+++ code.js
@@ -4,5 +4,22 @@ function helloWorld() {
 }
 
 function sayHello(input) {
-    return "Hello, " + input + "!";
+    if (input === Number){
+        return "Hello, World!";
+    }
+    if (input === null){
+        return "Hello, World!";
+    }
+    if (input === undefined){
+        return "Hello, World!";
+    }
+    if (input === true){
+        return "Hello, World!";
+    }
+    if (input === false){
+        return "Hello, World!";
+    }
+    else {
+        return "Hello, " + input + "!";
+    }
 }
